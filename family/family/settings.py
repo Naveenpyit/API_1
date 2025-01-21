b0ffc8135f58f96a3770037c2e101ae13b385cab
Naveenpyit <naveenpyit@gmail.com>
2025-01-21 10:52:38 +0530

    feat:small changes in set.py

--- family/family/settings.py
+++ family/family/settings.py
@@ -76,19 +76,20 @@ TEMPLATES = [
 WSGI_APPLICATION = 'family.wsgi.application'
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
+    'DEFAULT_AUTHENTICATION_CLASSES':[
 
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ]
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',  
+    ],
 }
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "AUTH_HEADER_TYPES": ("Bearer",),
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',  # Ensures users must be authenticated
-    ],
+    
 }
 
 # Database
